0215c46a2728ad16bb8048a337254975aaeb21a3
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2023-01-15 22:09:33 +0100

    Add files via upload

---
+++ MyCustomExtensions/MyExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MyCustomExtensions
+{
+    public static class MyExtensions
+    {
+        //oy2nijzqa2prbke6kdja2bvibtierguevfos3u32iirlpu
+        public static void Display<T>(T t) => Console.WriteLine(t);
+        public static void ProcessItems<T>(this IEnumerable<T> items)
+        {
+            switch (items)
+            {
+                case IEnumerable<int> ints:
+                    Integers();
+                    break;
+                case IEnumerable<string> strings:
+                    Strings();
+                    break;
+                default:
+                    Display("Invalid input.");
+                    break;
+            }
+
+
+            void Integers()
+            {
+                var positiveNumbers = items.OfType<int>().Where(x => x > 0);
+
+                if (positiveNumbers.Any())
+                {
+                    var sum = positiveNumbers.Sum();
+
+                    Display(sum);
+                }
+                else
+                {
+                    Display("No positive integers found!");
+                }
+            }
+
+
+            void Strings()
+            {
+                foreach (var item in items)
+                {
+                    Display(item);
+                }
+            }
+
+        }
+    }
+}
