cbe837b9a85b1fbc9e203707f528fc4804e9593d
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2023-01-17 10:14:48 +0100

    Update Tester.cs

--- MyCustomExtensions/Tester.cs
+++ MyCustomExtensions/Tester.cs
@@ -1,7 +1,45 @@
 //using MyCustomExceptions;
+//namespace Run;
 
-//var numbers = new List<int>() {1, -5, 8, 16};
-//numbers.ProcessItems();
+//internal class Program
+//{
+    //public static void Main(string[] args)
+    //{
+          //list
+          //var numbers = new List<int>() { 1, -1, 4, 2, 6 };
+          //numbers.ProcessItems();
 
-//var words = new List<string>() {"Obiagu", "Abakpa", "Garriki"};
-//words.ProcessItems();
+
+          //array
+          //var numbers2 = new int[] { 1, -1, 4, 2, 6 };
+          //numbers2.ProcessItems();
+
+
+          //sortedset
+          //SortedSet<int> numbers3 = new(new[] { 1, -1, 4, 2, 6 });
+          //numbers3.ProcessItems();
+
+
+          //hashset
+          //HashSet<int> numbers4 = new(new[] { 1, -1, 4, 2, 6 });
+          //numbers4.ProcessItems();
+
+
+          //collection
+          //Collection<int> ints = new(new int[] { 1, -1, 4, 2, 6 });
+          //ints.ProcessItems();
+            
+
+          //var strings = new List<string>() { "Abakpa", "Garriki", "Obiagu" };
+          //strings.ProcessItems();
+
+          //var strings2 = new string[] { "Abakpa", "Garriki", "Obiagu" };
+          //strings2.ProcessItems();
+
+          //will work with basically any type that inherits from IEnumerable, including lists, arrays, sorted sets etc.
+
+    //}
+  
+//}
+
+
